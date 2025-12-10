e11a7e1b219f53f0af1becb8882b8196acca220f
pqpq1560-ui <pqpq1560@gmail.com>
2025-12-10 20:28:56 +0900

    selfstudy

---
+++ ch11_1/src/exam1/MathRandomExample.java
@@ -0,0 +1,10 @@
+package exam1;
+
+public class MathRandomExample {
+	public static void main(String[] args) {
+		int num = (int) (Math.random()*6) + 1;
+		System.out.println("주사위 눈: " + num);
+
+	}
+
+}
